35a9368f4804db5f20d389aace27fd643f399b02
Matthew McKenzie <mmckenzie13@users.noreply.github.com>
2021-08-14 16:33:11 -0500

    Create SPO.ps1
    Initial Doc

---
+++ SPO.ps1
@@ -0,0 +1,25 @@
+## Sharepoint Online Migration - https://docs.microsoft.com/en-us/sharepointmigration/overview-spmt-ps-cmdlets
+
+## Register a Sharepoint Online Migration - https://docs.microsoft.com/en-us/powershell/module/spmt/register-spmtmigration
+Register-SPMTMigration
+
+## Add a migration task - https://docs.microsoft.com/en-us/powershell/module/spmt/Add-SPMTTask
+Add-SPMTTask
+
+## Remove a task - https://docs.microsoft.com/en-us/powershell/module/spmt/Remove-SPMTtask
+Remove-SPMTTask
+
+## Start your migration - https://docs.microsoft.com/en-us/powershell/module/spmt/start-spmtmigration
+Start-SPMTMigration
+
+## Return the object of current session - https://docs.microsoft.com/en-us/powershell/module/spmt/Get-SPMTMigration
+Get-SPMTMigration
+
+## Stop your current migration - https://docs.microsoft.com/en-us/powershell/module/spmt/Stop-SPMTMigration
+Stop-SPMTMigration
+
+## Show your migration status details in the console - https://docs.microsoft.com/en-us/powershell/module/spmt/Show-SPMTMigration
+Show-SPMTMigration
+
+## Remove the migration session - https://docs.microsoft.com/en-us/powershell/module/spmt/Unregister-SPMTMigration
+Unregister-SPMTMigration
